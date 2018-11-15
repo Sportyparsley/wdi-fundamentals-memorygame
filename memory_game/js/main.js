8394669ac666768bcdf3a49d1cd3922500537097
seb yeates <sebyeates@sebs-air.home>
2018-11-15 20:14:45 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,24 @@
 console.log("Up and running!");
+
 var cardOne = "queen";
 var cardTwo = "queen";
-var cardThree = "King";
-var cardFour = "King";
+var cardThree = "king";
+var cardFour = "king";
+
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree);
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0]
+cardsInPlay.push("queen");
+
+var cardTwo = cards[2]
+cardsInPlay.push("king");
+
+if (cardsInPlay.length === 2) & (cardsInPlay [0] === cardsInPlay[1]) || (cardsInPlay [2] === cardsInPlay[3]) {
+alert("You found a match!");
+} else { 
+alert("Sorry, try again.");
+}
